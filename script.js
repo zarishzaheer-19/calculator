bc65dfd18e534bb604ee880e17e59217d7ba8d54
zarishzaheer-19 <zarishzaheer19@gmail.com>
2026-07-17 11:25:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ display.value = "";
 function deleteLast() {
   display.value =display.value.slice(0, -1);
 }
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function changeTheme() {
+    const theme = document.getElementById("theme").value;
+
+    if (theme === "pink") {
+        document.body.style.background = "linear-gradient(135deg,#ffd6e8,#ffeef6)";
+    }
+
+    if (theme === "purple") {
+        document.body.style.background = "linear-gradient(135deg,#e9d5ff,#f5ebff)";
+    }
+
+    if (theme === "blue") {
+        document.body.style.background = "linear-gradient(135deg,#dbeafe,#eff6ff)";
+    }
+
+    if (theme === "mint") {
+        document.body.style.background = "linear-gradient(135deg,#d1fae5,#ecfdf5)";
+    }
+
+    if (theme === "dark") {
+        document.body.style.background = "#1e1e2f";
+    }
+}
