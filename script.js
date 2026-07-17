be82bd6e603db8b6275fd25ce803f1b598efc63f
zarishzaheer-19 <zarishzaheer19@gmail.com>
2026-07-17 11:38:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,29 +13,39 @@ function calculate() {
         display.value = eval(display.value);
     } catch {
         display.value = "Error";
-    }
-}
+    }function changeTheme() {
 
-function changeTheme() {
     const theme = document.getElementById("theme").value;
+    const calculator = document.querySelector(".calculator");
 
-    if (theme === "pink") {
+    if(theme === "pink"){
         document.body.style.background = "linear-gradient(135deg,#ffd6e8,#ffeef6)";
+        calculator.style.background = "#ffffff";
+        document.getElementById("display").style.color = "#ff4d94";
     }
 
-    if (theme === "purple") {
-        document.body.style.background = "linear-gradient(135deg,#e9d5ff,#f5ebff)";
+    else if(theme === "purple"){
+        document.body.style.background = "linear-gradient(135deg,#e9d5ff,#f3e8ff)";
+        calculator.style.background = "#ffffff";
+        document.getElementById("display").style.color = "#9333ea";
     }
 
-    if (theme === "blue") {
+    else if(theme === "blue"){
         document.body.style.background = "linear-gradient(135deg,#dbeafe,#eff6ff)";
+        calculator.style.background = "#ffffff";
+        document.getElementById("display").style.color = "#2563eb";
     }
 
-    if (theme === "mint") {
+    else if(theme === "mint"){
         document.body.style.background = "linear-gradient(135deg,#d1fae5,#ecfdf5)";
+        calculator.style.background = "#ffffff";
+        document.getElementById("display").style.color = "#059669";
     }
 
-    if (theme === "dark") {
-        document.body.style.background = "#1e1e2f";
+    else if(theme === "dark"){
+        document.body.style.background = "#1f2937";
+        calculator.style.background = "#374151";
+        document.getElementById("display").style.color = "#ffffff";
     }
-}
+
+    }
